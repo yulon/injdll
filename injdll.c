ee1d1fc28f5accc4f29fe0daa3561c293de1063c
Yulon <yulon@me.com>
2018-11-14 21:05:45 +0800

    For OSCZB 19034073

--- injdll.c
+++ injdll.c
@@ -4,26 +4,6 @@
 #include <windows.h>
 #include <tlhelp32.h>
 
-DWORD get_pid(LPCTSTR name) {
-	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (snapshot == INVALID_HANDLE_VALUE) {
-		return 0;
-	}
-
-	PROCESSENTRY32 pentry;
-	pentry.dwSize = sizeof(PROCESSENTRY32);
-	Process32First(snapshot, &pentry);
-	DWORD pid = 0;
-	do {
-		if (!lstrcmp(pentry.szExeFile, name)) {
-			pid = pentry.th32ProcessID;
-			break;
-		}
-	} while (Process32Next(snapshot, &pentry));
-	CloseHandle(snapshot);
-	return pid;
-}
-
 #define _ERROR(n) \
 	switch (n) { \
 		case 1: \
@@ -48,14 +28,11 @@ int main(int argc, char const *argv[]) {
 	char const **av = malloc(argc);
 
 	bool free_mode = false;
-	bool not_found_wait = false;
 
 	for (size_t i = 1; i < argc; i++) {
 		if (argv[i][0] == '-') {
 			if (!strcmp(argv[i], "-f") || !strcmp(argv[i], "--free")) {
 				free_mode = true;
-			} else if (!strcmp(argv[i], "-w") || !strcmp(argv[i], "--not-found-wait")) {
-				not_found_wait = true;
 			}
 		} else {
 			av[ac++] = argv[i];
@@ -63,55 +40,15 @@ int main(int argc, char const *argv[]) {
 	}
 
 	if (ac < 2) {
-		puts("error!");
 		_ERROR(1);
 	}
 
-	HANDLE h = NULL;
-	HANDLE th = NULL;
-
-	if (av[0][0] == '*') {
-		STARTUPINFOA si;
-		memset(&si, 0, sizeof(si));
-		si.cb = sizeof(si);
-
-		PROCESS_INFORMATION pi;
-		memset(&pi, 0, sizeof(pi));
-
-		if (CreateProcessA(
-			NULL,
-			(char *)&av[0][1],
-			NULL,
-			NULL,
-			true,
-			CREATE_SUSPENDED,
-			NULL,
-			NULL,
-			&si,
-			&pi
-		)) {
-			h = pi.hProcess;
-			th = pi.hThread;
-		} else {
-			CloseHandle(pi.hProcess);
-			CloseHandle(pi.hThread);
-		}
-	} else {
-		DWORD pid = get_pid(av[0]);
-		if (!pid) {
-			if (not_found_wait) {
-				do {
-					Sleep(1000);
-					pid = get_pid(av[0]);
-				} while (!pid);
-				Sleep(5000);
-			} else {
-				_ERROR(2);
-			}
-		}
-		h = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, pid);
+	DWORD pid = atol(av[0]);
+	if (!pid) {
+		_ERROR(2);
 	}
 
+	HANDLE h = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, pid);
 	if (!h) {
 		_ERROR(2);
 	}
@@ -167,13 +104,6 @@ int main(int argc, char const *argv[]) {
 	DWORD pthrd_r;
 	GetExitCodeThread(pthrd, &pthrd_r);
 	CloseHandle(pthrd);
-
-	printf("result: %d\n", pthrd_r);
-
-	if (th) {
-		ResumeThread(th);
-		CloseHandle(th);
-	}
 
 	CloseHandle(h);
 	return 0;
